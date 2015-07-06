031d8eb49dc78e9b0218463bf99c6e17e84ad7bb
Joshua Kittrell <whiteflags99@gmail.com>
2015-07-06 15:28:33 -0400

    Form layout changes for new features

--- jkod/jkod.Designer.cs
+++ jkod/jkod.Designer.cs
@@ -32,8 +32,12 @@
             this.btnOpen = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.cbxBaseList = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.cbxBaseList = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbxColumnWidth = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbxBytesPerLine = new System.Windows.Forms.ComboBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -60,6 +64,10 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.cbxBytesPerLine);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cbxColumnWidth);
+            this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.cbxBaseList);
             this.groupBox1.Location = new System.Drawing.Point(12, 359);
@@ -68,6 +76,15 @@
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Options";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Base:";
             // 
             // cbxBaseList
             // 
@@ -80,14 +97,49 @@
             this.cbxBaseList.Size = new System.Drawing.Size(179, 21);
             this.cbxBaseList.TabIndex = 0;
             // 
-            // label1
+            // label2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 27);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(34, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Base:";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(210, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Column Width (bytes):";
+            // 
+            // cbxColumnWidth
+            // 
+            this.cbxColumnWidth.FormattingEnabled = true;
+            this.cbxColumnWidth.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "4",
+            "8"});
+            this.cbxColumnWidth.Location = new System.Drawing.Point(213, 43);
+            this.cbxColumnWidth.Name = "cbxColumnWidth";
+            this.cbxColumnWidth.Size = new System.Drawing.Size(73, 21);
+            this.cbxColumnWidth.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(347, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Bytes Per Line:";
+            // 
+            // cbxBytesPerLine
+            // 
+            this.cbxBytesPerLine.FormattingEnabled = true;
+            this.cbxBytesPerLine.Items.AddRange(new object[] {
+            "16",
+            "32",
+            "64",
+            "128"});
+            this.cbxBytesPerLine.Location = new System.Drawing.Point(350, 43);
+            this.cbxBytesPerLine.Name = "cbxBytesPerLine";
+            this.cbxBytesPerLine.Size = new System.Drawing.Size(121, 21);
+            this.cbxBytesPerLine.TabIndex = 7;
             // 
             // jkod
             // 
@@ -115,6 +167,10 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox cbxBaseList;
+        private System.Windows.Forms.ComboBox cbxBytesPerLine;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbxColumnWidth;
+        private System.Windows.Forms.Label label2;
     }
 }
 
